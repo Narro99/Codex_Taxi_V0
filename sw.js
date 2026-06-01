63c4806bd4516053a750b98455fcff38dcb0f685
Narro99 <daninarro99@gmail.com>
2026-06-01 21:38:48 +0200

    Refresh web cache for Pages

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const CACHE = 'taxi-empresa-v1';
+const CACHE = 'taxi-empresa-v2';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
